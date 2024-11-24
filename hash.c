eeecedcec5d7fc0362c2d17a96c2f5b3d6917b0d
JungHyunLee <aktlakfhjh2@gmail.com>
2024-11-24 13:51:29 +0900

    add queue

--- hash.c
+++ hash.c
@@ -12,17 +12,40 @@
 #define PORTNUM 9100 
 #define MAX_CLIENTS 100
 #define NUM_SERVERS 1
+#define QUEUE_SIZE 20
 
 typedef struct {
-    char ip[16]; 
-    int port;   
+    char ip[16];
+    int port;
 } server_info;
 
+typedef struct {
+    int client_socket;
+} client_request;
+
+typedef struct {
+    client_request requests[QUEUE_SIZE];
+    int front, rear, count;
+    pthread_mutex_t mutex;
+    pthread_cond_t cond_non_empty;
+    pthread_cond_t cond_non_full;
+} request_queue;
+
 server_info web_servers[] = {
-    {"", PORTNUM},
-    {"", PORTNUM},
-    {"", PORTNUM}
+    {"10.198.138.212", PORTNUM},
+    {"10.198.138.213", PORTNUM}
+
 };
+
+request_queue queue = {
+    .front = 0,
+    .rear = 0,
+    .count = 0,
+    .mutex = PTHREAD_MUTEX_INITIALIZER,
+    .cond_non_empty = PTHREAD_COND_INITIALIZER,
+    .cond_non_full = PTHREAD_COND_INITIALIZER
+};
+
 unsigned int murmur_hash(char* key) {
     unsigned int seed = 0x1234abcd;
     unsigned int m = 0x5bd1e995;
@@ -56,73 +79,98 @@ int load_balance(char* client_ip) {
     return murmur_hash(client_ip);
 }
 
-void* handle_client(void* client_sock) {
-    int client_socket = *(int*)client_sock;
-    free(client_sock);
-
-    char client_ip[16];
-    struct sockaddr_in addr;
-    socklen_t addr_len = sizeof(addr);
-    if (getpeername(client_socket, (struct sockaddr*)&addr, &addr_len) == 0) {
-        strcpy(client_ip, inet_ntoa(addr.sin_addr));
+void enqueue(int client_socket) {
+    pthread_mutex_lock(&queue.mutex);
+    while (queue.count == QUEUE_SIZE) {
+        pthread_cond_wait(&queue.cond_non_full, &queue.mutex);
     }
-    else {
-        strcpy(client_ip, "Unknown");
+    queue.requests[queue.rear].client_socket = client_socket;
+    queue.rear = (queue.rear + 1) % QUEUE_SIZE;
+    queue.count++;
+    pthread_cond_signal(&queue.cond_non_empty);
+    pthread_mutex_unlock(&queue.mutex);
+}
+
+int dequeue() {
+    pthread_mutex_lock(&queue.mutex);
+    while (queue.count == 0) {
+        pthread_cond_wait(&queue.cond_non_empty, &queue.mutex);
     }
+    int client_socket = queue.requests[queue.front].client_socket;
+    queue.front = (queue.front + 1) % QUEUE_SIZE;
+    queue.count--;
+    pthread_cond_signal(&queue.cond_non_full);
+    pthread_mutex_unlock(&queue.mutex);
+    return client_socket;
+}
 
-    int server_index = load_balance(client_ip);
-    server_info selected_server = web_servers[server_index];
+void* handle_client(void* arg) {
+    while (1) {
+        int client_socket = dequeue();
 
-    int server_socket;
-    struct sockaddr_in server_addr;
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1) {
-        perror("Socket creation failed for server");
-        close(client_socket);
-        return NULL;
-    }
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(selected_server.port);
-    inet_pton(AF_INET, selected_server.ip, &server_addr.sin_addr);
+        char client_ip[16];
+        struct sockaddr_in addr;
+        socklen_t addr_len = sizeof(addr);
+        if (getpeername(client_socket, (struct sockaddr*)&addr, &addr_len) == 0) {
+            strcpy(client_ip, inet_ntoa(addr.sin_addr));
+        }
+        else {
+            strcpy(client_ip, "Unknown");
+        }
 
-    if (connect(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        perror("connect");
+        int server_index = load_balance(client_ip);
+        server_info selected_server = web_servers[server_index];
+
+        int server_socket;
+        struct sockaddr_in server_addr;
+        server_socket = socket(AF_INET, SOCK_STREAM, 0);
+        if (server_socket == -1) {
+            perror("Socket creation failed for server");
+            close(client_socket);
+            continue;
+        }
+        memset(&server_addr, 0, sizeof(server_addr));
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons(selected_server.port);
+        inet_pton(AF_INET, selected_server.ip, &server_addr.sin_addr);
+
+        if (connect(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+            perror("connect");
+            close(client_socket);
+            close(server_socket);
+            continue;
+        }
+
+        char buffer[1024];
+        int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
+        if (bytes_received > 0) {
+            send(server_socket, buffer, bytes_received, 0);
+        }
+        else if (bytes_received < 0) {
+            perror("recv");
+            close(client_socket);
+            close(server_socket);
+            continue;
+        }
+
+        bytes_received = recv(server_socket, buffer, sizeof(buffer), 0);
+        if (bytes_received > 0) {
+            send(client_socket, buffer, bytes_received, 0);
+        }
+        else if (bytes_received < 0) {
+            perror("recv");
+        }
+
         close(client_socket);
         close(server_socket);
-        return NULL;
     }
-
-    char buffer[1024];
-    int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
-    if (bytes_received > 0) {
-        send(server_socket, buffer, bytes_received, 0);
-    }
-    else if (bytes_received < 0) {
-        perror("recv");
-        close(client_socket);
-        close(server_socket);
-        return NULL;
-    }
-
-    bytes_received = recv(server_socket, buffer, sizeof(buffer), 0);
-    if (bytes_received > 0) {
-        send(client_socket, buffer, bytes_received, 0);
-    }
-    else if (bytes_received < 0) {
-        perror("recv");
-    }
-
-    close(client_socket);
-    close(server_socket);
     return NULL;
 }
 
 int main() {
-    int server_socket, * new_sock;
+    int server_socket;
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_addr_len = sizeof(client_addr);
-
 
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket < 0) {
@@ -141,20 +189,18 @@ int main() {
 
     listen(server_socket, MAX_CLIENTS);
 
+    pthread_t tids[5];
+    for (int i = 0; i < 5; i++) {
+        pthread_create(&tids[i], NULL, handle_client, NULL);
+    }
+
     while (1) {
         int client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_len);
         if (client_socket < 0) {
             perror("accept");
             continue;
         }
-        
-        pthread_t tid;
-        new_sock = malloc(sizeof(int));
-        *new_sock = client_socket;
-        if (pthread_create(&tid, NULL, handle_client, (void*)new_sock) != 0) {
-            close(client_socket);
-            free(new_sock);
-        }
+        enqueue(client_socket);
     }
 
     close(server_socket);
